7aaeaf8e09ac5dd572f8dc828b7cffd36105f11c
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2024-01-18 10:44:16 +0100

    🔨 refactor(app.json): add Norwegian translation for "Smart Electronic Door Lock" 🔨 refactor(app.json): update clusters and bindings for "Smart Electronic Door Lock" 🔨 refactor(device.js): update method signatures and logging statements 🔨 refactor(driver.compose.json): add Norwegian translation for "Smart Electronic Door Lock" 🔨 refactor(package.json): remove unnecessary eslint dependencies

--- drivers/HT-SLM-2/device.js
+++ drivers/HT-SLM-2/device.js
@@ -1,87 +1,71 @@
 'use strict';
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { CLUSTER } = require('zigbee-clusters');
-const { debug } = require('zigbee-clusters');
+const { DoorLockCluster, CLUSTER, debug } = require('zigbee-clusters');
 
 // Enable debug logging of all relevant Zigbee communication
 debug(true);
 
 class HTSLM2 extends ZigBeeDevice {
 
-  async onNodeInit({ zclNode, node }) {
-    this.log('HT-SLM-2 Node has been initialized');
+    async onNodeInit({ zclNode }) {
+        this.log('HT-SLM-2 Node has been initialized');
 
-    this.log('Node:', node);
+        this.enableDebug();
+        this.printNode();
 
-    this.printNode();
+        if (this.isFirstInit()) {
+            // Register measure_battery capability and configure attribute reporting
+            this.batteryThreshold = 20;
+            await this.configureAttributeReporting([
+                {
+                    endpointId: 1,
+                    cluster: CLUSTER.POWER_CONFIGURATION,
+                    attributeName: 'batteryPercentageRemaining',
+                    minInterval: 0, //??
+                    maxInterval: 1500,
+                    minChange: 0
+                },
+            ]);
+        }
+        // measure_battery 
+        zclNode.endpoints[1].clusters[CLUSTER.POWER_CONFIGURATION.NAME].on('attr.batteryPercentageRemaining', this.onBatteryPercentageRemainingAttributeReport.bind(this));
 
-    if (this.isFirstInit()) {
-      await this.configureAttributeReporting([{
-        endpointId: 1,
-        cluster: CLUSTER.POWER_CONFIGURATION,
-        attributeName: 'batteryPercentageRemaining',
-        minInterval: 65535,
-        maxInterval: 0,
-        minChange: 0,
-      }]);
+        this.log(CLUSTER.DOOR_LOCK);
     }
 
-    // measure_battery // alarm_battery
-    zclNode.endpoints[1].clusters[CLUSTER.POWER_CONFIGURATION.NAME]
-      .on('attr.batteryPercentageRemaining', (value) => {
-        this.log('measured batt', value);
-        this.setCapabilityValue('measure_battery', value);
-      });
-
-    try {
-      // Abonner på endringer i doorLock-klyngen
-      const doorLockCluster = zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME];
-      if (doorLockCluster) {
-        doorLockCluster.on('attr.lockState', this.onLockStateChange.bind(this));
-      }
-
-      // Debug: Sjekk tilgjengeligheten av doorLock-klyngen
-      if (zclNode.endpoints[1].clusters.doorLock) {
-        this.log('doorLock cluster is available');
-      } else {
-        this.log('doorLock cluster is NOT available');
-      }
-
-      this.log('Door Lock Cluster Name:', CLUSTER.DOOR_LOCK.NAME);
-
-      // Debug: Logg hele zclNode-objektet for å se detaljene
-      this.log('zclNode:', zclNode);
-    } catch (error) {
-      this.log('Error in onNodeInit:', error);
+    // Håndterer endringer i låsens tilstand
+    onLockStateChange(value) {
+        this.log('Lock state changed:', value);
+        this.setCapabilityValue('locked', value === 'locked');
     }
-  }
 
-  // Håndterer endringer i låsens tilstand
-  onLockStateChange(value) {
-    this.log('Lock state changed:', value);
-    this.setCapabilityValue('locked', value === 'locked');
-  }
+    onEndDeviceAnnounce() {
+        this.log('device came online!');
+    }
 
-  async onInit() {
-    this.log('HT-SLM-2 has been initialized');
-  }
+    onBatteryPercentageRemainingAttributeReport(batteryPercentageRemaining) {
+        this.log("measure_battery | powerConfiguration - batteryPercentageRemaining (%): ", batteryPercentageRemaining / 2);
+        //const batteryThreshold = this.getSetting('batteryThreshold') || 20;
+        this.setCapabilityValue('measure_battery', batteryPercentageRemaining / 2).catch(this.error);
+        //this.setCapabilityValue('alarm_battery', (batteryPercentageRemaining/2 < batteryThreshold) ? true : false).catch(this.error);
+    }
 
-  async onAdded() {
-    this.log('HT-SLM-2 has been added');
-  }
+    async onAdded() {
+        this.log('HT-SLM-2 has been added');
+    }
 
-  async onSettings({ oldSettings, newSettings, changedKeys }) {
-    this.log('HT-SLM-2 settings where changed');
-  }
+    async onSettings({ oldSettings, newSettings, changedKeys }) {
+        this.log('HT-SLM-2 settings where changed');
+    }
 
-  async onRenamed(name) {
-    this.log('HT-SLM-2 was renamed');
-  }
+    async onRenamed(name) {
+        this.log('HT-SLM-2 was renamed');
+    }
 
-  async onDeleted() {
-    this.log('HT-SLM-2 has been deleted');
-  }
+    async onDeleted() {
+        this.log('HT-SLM-2 has been deleted');
+    }
 
 }
 module.exports = HTSLM2;
