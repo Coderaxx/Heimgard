ecc15b9e4b75e26e7eeb10fcf17a93a3b8d9f016
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2024-01-28 21:02:01 +0100

    Update Heimgard Smart Electronic Door Lock and app.json

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
-Welcome to Heimgard. Simple, flexible and secure.
-
+Introducing the Heimgard Smart Lock App for Homey Pro: Security and Smart Control in Your Home"
+Discover the future of smart home security with the Heimgard app for Homey Pro, specifically designed to integrate with Heimgard’s smart door lock. This app represents a significant advancement in smart home technology, tailored to give you complete control over your home's security. With the Heimgard app, you can effortlessly lock and unlock your doors, manage access, and monitor door activity right from your Homey Pro. Our app combines Heimgard’s renowned security standards with the user-friendliness of Homey Pro, creating a seamless and secure smart home experience. Whether you are at home or away, the Heimgard app gives you peace of mind, knowing your home is safe and secure. Join the Heimgard family and enjoy the ultimate convenience and security that our smart lock app brings to your home.
--- drivers/HT-SLM-2/device.js
+++ drivers/HT-SLM-2/device.js
@@ -2,9 +2,13 @@
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
 const { Cluster, CLUSTER, debug } = require('zigbee-clusters');
+const LockControlBoundCluster = require('../../lib/LockControlBoundCluster');
 
 // Enable debug logging of all relevant Zigbee communication
 debug(true);
+
+const HeimgardDoorLockCluster = require('../../lib/HeimgardDoorLockCluster');
+Cluster.addCluster(HeimgardDoorLockCluster);
 
 class HTSLM2 extends ZigBeeDevice {
 
@@ -14,18 +18,114 @@ class HTSLM2 extends ZigBeeDevice {
         this.enableDebug();
         this.printNode();
 
-        //this.log(await zclNode.endpoints[1].clusters.doorLock.discoverAttributesExtended());
-        this.log(await zclNode.endpoints[1].clusters.doorLock.readReportingConfiguration([27, 36, 40]));
-        this.log(await zclNode.endpoints[1].clusters.doorLock.discoverCommandsGenerated());
-        this.log(await zclNode.endpoints[1].clusters.doorLock.discoverCommandsReceived());
+        //this.log('discoverAttributesExtended', await zclNode.endpoints[1].clusters.doorLock.discoverAttributesExtended());
+        //this.log('readReportingConfiguration', await zclNode.endpoints[1].clusters.doorLock.readReportingConfiguration(["lockState", "lockType", "actuatorEnabled", "doorState", "doorOpenEvents", "doorClosedEvents"]));
+        /*const currentLockState = await zclNode.endpoints[1].clusters.doorLock.readAttributes(
+            ["lockState", "lockType", "actuatorEnabled", "doorState", "soundVolume", "language", "numberOfTotalUsersSupported", "enableLocalProgramming"],
+        ).catch(err => { this.error(err); });
+        this.log('Door Lock State: ' + JSON.stringify(currentLockState, null, 2));*/
 
-        /*this.registerCapability('measure_battery', CLUSTER.POWER_CONFIGURATION, {
+        this.log('discoverCommandsGenerated', await zclNode.endpoints[1].clusters.doorLock.discoverCommandsGenerated());
+        this.log('discoverCommandsReceived', await zclNode.endpoints[1].clusters.doorLock.discoverCommandsReceived());
+
+        this.registerCapability('locked', CLUSTER.DOOR_LOCK, {
+            set: value => (value ? 'lockDoor' : 'unlockDoor'),
+            setParser(setValue) {
+                this.log('setParser', setValue);
+                if (setValue === 'lockDoor') return true;
+                return false;
+            },
+            get: 'lockState',
+            report: 'lockState',
+            reportParser(report) {
+                this.log('reportParser', report);
+                if (report && report === 'locked') return true;
+                return false;
+            },
+            reportOpts: {
+                configureAttributeReporting: {
+                    minInterval: 0, // Minimally once every hour
+                    maxInterval: 60 * 60 * 1000, // Maximally once every ~16 hours
+                    minChange: 0,
+                },
+            },
+            endpoint: 1, // Default is 1
+            getOpts: {
+                getOnStart: true,
+                getOnOnline: true,
+                pollInterval: 1000 * 1000, // in ms
+            },
+        });
+
+        await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].on('response', async (data) => {
+            this.log('response', data);
+        });
+
+        zclNode.endpoints[1].bind(
+            CLUSTER.DOOR_LOCK.NAME, new LockControlBoundCluster({
+                onLock: (payload) => {
+                    this.log('onLock', payload);
+                    this.setCapabilityValue('locked', true);
+                },
+                onUnlock: (payload) => {
+                    this.log('onUnlock', payload);
+                    this.setCapabilityValue('locked', false);
+                },
+                unlockWithTimeout: (payload) => {
+                    this.log('unlockWithTimeout', payload);
+                }
+            })
+        );
+
+        /*this.registerCapability('measure_voltage', CLUSTER.POWER_CONFIGURATION, {
+            get: 'batteryVoltage',
+            report: 'batteryVoltage',
+            reportParser(report) {
+                if (report) {
+                    return report / 10;
+                }
+            },
+            reportOpts: {
+                configureAttributeReporting: {
+                    minInterval: 0, // Minimally once every hour
+                    maxInterval: 10800 * 1000, // Maximally once every ~16 hours
+                    minChange: 1,
+                },
+            },
+            endpoint: 1, // Default is 1
+            getOpts: {
+                getOnStart: true,
+                getOnOnline: true,
+                pollInterval: 30 * 1000, // in ms
+            },
+        });
+
+        if (this.hasCapability('alarm_battery')) {
+            // Set battery threshold under which the alarm should go off
+            this.batteryThreshold = 20;
+
+            // Register measure_battery capability and configure attribute reporting
+            this.registerCapability('alarm_battery', CLUSTER.POWER_CONFIGURATION, {
+                getOpts: {
+                    getOnStart: true,
+                },
+                reportOpts: {
+                    configureAttributeReporting: {
+                        minInterval: 0, // No minimum reporting interval
+                        maxInterval: 10800 * 1000, // Maximally every ~16 hours
+                        minChange: 5, // Report when value changed by 5
+                    },
+                },
+            });
+        }
+
+        this.registerCapability('measure_battery', CLUSTER.POWER_CONFIGURATION, {
             get: 'batteryPercentageRemaining',
             report: 'batteryPercentageRemaining',
             reportOpts: {
                 configureAttributeReporting: {
                     minInterval: 0, // Minimally once every hour
-                    maxInterval: 60000, // Maximally once every ~16 hours
+                    maxInterval: 10800 * 1000, // Maximally once every ~16 hours
                     minChange: 1,
                 },
             },
@@ -33,29 +133,9 @@ class HTSLM2 extends ZigBeeDevice {
             getOpts: {
                 getOnStart: true,
                 getOnOnline: true,
-                pollInterval: 30000, // in ms
+                pollInterval: 30 * 1000, // in ms
             },
         });*/
-
-        /*this.registerCapability('locked', CLUSTER.DOOR_LOCK, {
-            get: 36,
-            report: 36,
-            reportOpts: {
-                configureAttributeReporting: {
-                    minInterval: 0, // Minimally once every hour
-                    maxInterval: 60000, // Maximally once every ~16 hours
-                    minChange: 1,
-                },
-            },
-            endpoint: 1, // Default is 1
-            getOpts: {
-                getOnStart: true,
-                getOnOnline: true,
-                pollInterval: 30000, // in ms
-            },
-        });*/
-
-        this.log(CLUSTER.DOOR_LOCK);
     }
 
     async onAdded() {
@@ -63,7 +143,7 @@ class HTSLM2 extends ZigBeeDevice {
     }
 
     async onSettings({ oldSettings, newSettings, changedKeys }) {
-        this.log('HT-SLM-2 settings where changed');
+        this.log('HT-SLM-2 settings were changed');
     }
 
     async onRenamed(name) {
