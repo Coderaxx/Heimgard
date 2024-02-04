396b544a82605f0eab42542797a89ba3002973bb
Jesper Grimstad <48462275+Coderaxx@users.noreply.github.com>
2024-02-04 15:09:07 +0100

    Update HeimgardDoorLockCluster and app.json

--- drivers/HT-SLM-2/device.js
+++ drivers/HT-SLM-2/device.js
@@ -10,6 +10,7 @@ debug(true);
 
 class HTSLM2 extends ZigBeeDevice {
     async onNodeInit({ zclNode }) {
+        this.homey.app.log('Started device initialization. Initializing node...', 'HT-SLM-2');
         this.settings = await this.getSettings();
         this.enableDebug();
         //this.printNode();
@@ -17,12 +18,6 @@ class HTSLM2 extends ZigBeeDevice {
         //this.log(await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].discoverCommandsGenerated());
         //this.log(await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].discoverCommandsReceived());
         //this.log(await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].discoverAttributesExtended());
-        /*await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].setPinCode({
-            userID: 5,
-            userStatus: 1,
-            userType: 0,
-            pinCode: Buffer.from('0589'),
-        }).catch(e => this.homey.app.log(e, 'HT-SLM-2', 'ERROR'));*/
 
         this.registerCapability('locked', CLUSTER.DOOR_LOCK, {
             set: value => (value ? 'lockDoor' : 'unlockDoor'),
@@ -94,8 +89,38 @@ class HTSLM2 extends ZigBeeDevice {
         zclNode.endpoints[1].clusters.doorLock.on('attr.soundVolume', this._onReport.bind(this));
         zclNode.endpoints[1].clusters.powerConfiguration.on('attr.batteryPercentageRemaining', this._onReport.bind(this));
         zclNode.endpoints[1].clusters.doorLock.on('event.operatingEventNotification', this._onOperatingEventNotification.bind(this));
+        zclNode.endpoints[1].clusters.doorLock.on('event.programmingEventNotification', this._onProgrammingEventNotification.bind(this));
+
+        await zclNode.endpoints[1].clusters[CLUSTER.DOOR_LOCK.NAME].getPinCode({
+            userID: 5,
+        }).then((res) => {
+            this.homey.app.log('Received response for getPinCode command:', 'HT-SLM-2', 'DEBUG', {
+                userID: res.userID,
+                userStatus: res.userStatus,
+                userType: res.userType,
+                pinCode: res.pinCode.toString(),
+            });
+        }).catch(e => this.homey.app.log(e, 'HT-SLM-2', 'ERROR'));
+
+        await this.initFlows();
 
         this.homey.app.log('HT-SLM-2 Node has been initialized', 'HT-SLM-2');
+    }
+
+    async initFlows() {
+        const setUserPIn = this.homey.flow.getActionCard('setUserPin');
+        setUserPIn.registerRunListener(async (args) => {
+            await this.zclNode.endpoints[1].clusters.doorLock.setPinCode({
+                userID: parseInt(args.userID) || args.userID,
+                userStatus: parseInt(args.userStatus) || args.userStatus,
+                userType: parseInt(args.userType) || args.userType,
+                pinCode: Buffer.from(`${args.pinCode}`),
+            }).then(() => {
+                this.homey.app.log(`Added a new user with ID ${args.userID} and PIN ${args.pinCode}. (UserType: ${args.userType} | UserStatus: ${args.userStatus})`, 'HT-SLM-2');
+            }).catch(e => this.homey.app.log('Failed to add a new user:', 'HT-SLM-2', 'ERROR', e));
+        });
+
+        this.homey.app.log('HT-SLM-2 flows were initialized', 'HT-SLM-2');
     }
 
     async onAdded() {
@@ -125,16 +150,7 @@ class HTSLM2 extends ZigBeeDevice {
             }).catch(e => this.homey.app.log(e, 'HT-SLM-2', 'ERROR'));
         }
 
-        await this.initFlows();
-
         this.homey.app.log('HT-SLM-2 settings were changed', 'HT-SLM-2');
-    }
-
-    async initFlows() {
-        const setUserPIn = this.homey.flow.getActionCard('setUserPin');
-        setUserPIn.registerRunListener(async (args, state) => {
-            this.log('User pin was set', args, state);
-        });
     }
 
     async onRenamed(name) {
@@ -149,8 +165,16 @@ class HTSLM2 extends ZigBeeDevice {
         this.homey.app.log('Received an operating event notification:', 'HT-SLM-2', 'DEBUG', payload);
     }
 
+    _onProgrammingEventNotification(payload) {
+        this.homey.app.log('Received a programming event notification:', 'HT-SLM-2', 'DEBUG', payload);
+    }
+
     _onReport(data) {
         this.homey.app.log('Received an attribute report:', 'HT-SLM-2', 'DEBUG', data);
+    }
+
+    _onCommand(data) {
+        this.homey.app.log('Received a command response:', 'HT-SLM-2', 'DEBUG', data);
     }
 
 }
